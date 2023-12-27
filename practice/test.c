f301c48ff675b8f32813822b272968e3bf214e60
BIKRAMJIT SARKAR <115685358+biku-tubai@users.noreply.github.com>
2023-12-27 15:12:04 +0530

    Add files via upload

---
+++ practice/test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    if(10)
+    {
+        printf("true\n");
+    }
+    if(0)
+    {
+        printf("false\n");
+    }
+    else if(1)
+    {
+        printf("no one\n");
+    }
+//   int a,b,c=10;
+//   a=c+(c==9);
+//   b=c+(c==10);
+//   printf("a=%d\nb=%d",a,b);
+  return 0;
+}
